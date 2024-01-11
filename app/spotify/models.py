fe357f5357ba4f5da5ff2d57e47a0b972da5dbb3
Mark Young <markyoung.ca@gmail.com>
2024-01-10 16:34:19 -0800

    Removed fields no longer needed from ListenLog

--- app/spotify/models.py
+++ app/spotify/models.py
@@ -7,31 +7,6 @@ ALBUM_TYPES = [
     ("ALBUM", "Album"),
 ]
 
-DAYS_OF_THE_WEEK = [
-    (6, "Sunday"),
-    (0, "Monday"),
-    (1, "Tuesday"),
-    (2, "Wednesday"),
-    (3, "Thursday"),
-    (4, "Friday"),
-    (5, "Saturday"),
-]
-
-MONTHS = [
-    (1, "January"),
-    (2, "February"),
-    (3, "March"),
-    (4, "April"),
-    (5, "May"),
-    (6, "June"),
-    (7, "July"),
-    (8, "August"),
-    (9, "September"),
-    (10, "October"),
-    (11, "November"),
-    (12, "December"),
-]
-
 
 class Artist(models.Model):
     artist_id = models.CharField(primary_key=True)
@@ -81,9 +56,6 @@ class ListenLog(models.Model):
     # POSIX timestamp is converted to a str in order to be a primary key
     played_at_posix_timestamp = models.CharField(primary_key=True)
     played_at_datetime = models.DateTimeField()
-    played_at_month = models.IntegerField(choices=MONTHS)
-    played_at_dow = models.CharField(choices=DAYS_OF_THE_WEEK)
-    played_at_hour = models.IntegerField()  # 24 Hour
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     track = models.ForeignKey(Track, on_delete=models.CASCADE)
 
